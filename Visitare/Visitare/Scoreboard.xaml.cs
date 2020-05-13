29ca7d5131b52eced1407af9e0a488c6c45d5888
Bartosz Michalski <bartek10114@wp.pl>
2020-05-13 17:03:05 +0200

    Testowanie przekazywania tokenu do metody get w scoreboard.xaml.cs

--- Visitare/Visitare/Scoreboard.xaml.cs
+++ Visitare/Visitare/Scoreboard.xaml.cs
@@ -32,7 +32,7 @@ namespace Visitare
 
             HttpClient client = new HttpClient();
 
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", " RHI069lPFC1wjDCXPzv5ywHtoJIhDsL6DH55ovfKzMKtGC3Z24bhJqh31AiN-38-9GedqSh78NVKklexIr2yPHHTSYaP2yffr8O4ZmLp-ZCRgH6IwLmYhgwWSSLr540oYPclhJhCyvhAkoEbqEiHf7EUmsqLCkBIxfchQrg9EpTlXoot7x0XfR5msaReothGCQrTKEpLA6YS12EKVXxdJMvgY0NwlN2G8EdCUPFh1pJVCIrx0R12uAl1KYG-o3RAvUVdKbph6TnSpaZ2UNhYJ7N0Lr0DLv-GydBwlLbTrygpPoF6GUHMex9UkHWTB4mHMELXwfB0YTx2Z9t87xOJPiKFpxplefuh2Z1CWRWucP8nG7J0JcL2O77RQ_Sd8sr-NNgWqBPKZXoRTiO8_-cZG4tmdyNQSqbtjakTdq9MGAmDjxxMLsWx6y_6tS4byRPQU67wkAorlQWszXXrTwTqQo01Ureofp__0y3ED7MwUjplrgCzCl_vQj0adcELjyUT");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
             var response = await client.GetStringAsync("http://dearjean.ddns.net:44301/api/Rewards/GetAll");
 
